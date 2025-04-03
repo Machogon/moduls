ce84d07bedf8fc6111e365efbeb285fe95626380
Machogon <83405388+Machogon@users.noreply.github.com>
2025-04-03 16:28:12 +0300

    UP category_modules.js

--- category_modules.js
+++ category_modules.js
@@ -33,10 +33,9 @@
          }
  
          // Особый случай: добавление обеих сторон если не найдена
-         if (paramId === "122881" && !result["122881"] && !this.skipSideIfNotFound && parameters["122881"]) {
-         if (!result["122881"] && !this.skipSideIfNotFound && this.parameters["122881"]) {
+         if (!result["122881"] && !this.skipSideIfNotFound && parameters["122881"]) {
              result["122881"] = ["Слева", "Справа"];
-             console.log(`ℹ Добавляем обе стороны установки, так как сторона не указана`);
+             console.log(`ℹ Добавляем обе стороны установки`);
          }
  
          return Object.keys(result).length ? result : null;
